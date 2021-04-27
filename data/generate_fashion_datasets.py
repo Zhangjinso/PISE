f9dfacb16db99420a5a51bf7e0fd2a632466b701
jinszhang <53162769+Zhangjinso@users.noreply.github.com>
2021-04-27 14:17:40 +0800

    Update generate_fashion_datasets.py

--- data/generate_fashion_datasets.py
+++ data/generate_fashion_datasets.py
@@ -13,54 +13,53 @@ def is_image_file(filename):
 def make_dataset(dir):
 	images = []
 	assert os.path.isdir(dir), '%s is not a valid directory' % dir
-	# new_root = './fashion'
-	# if not os.path.exists(new_root):
-	# 	os.mkdir(new_root)
+	new_root = './fashion_data'
+	if not os.path.exists(new_root):
+		os.mkdir(new_root)
 
-	train_root = os.path.join(dir, 'train')
+	train_root = './fashion_data/train'
 	if not os.path.exists(train_root):
 		os.mkdir(train_root)
 
-	test_root = os.path.join(dir, 'test')
+	test_root = './fashion_data/test'
 	if not os.path.exists(test_root):
 		os.mkdir(test_root)
 
 	train_images = []
-	train_f = open(os.path.join(dir, 'train.lst'), 'r')
+	train_f = open('./fashion_data/train.lst', 'r')
 	for lines in train_f:
 		lines = lines.strip()
 		if lines.endswith('.jpg'):
 			train_images.append(lines)
 
 	test_images = []
-	test_f = open(os.path.join(dir, 'test.lst'), 'r')
+	test_f = open('./fashion_data/test.lst', 'r')
 	for lines in test_f:
 		lines = lines.strip()
 		if lines.endswith('.jpg'):
 			test_images.append(lines)
 
+	print(train_images, test_images)
+
 	# print(train_images, test_images)
 
-	for root, _, fnames in sorted(os.walk(os.path.join(dir, 'img'))):
+	for root, _, fnames in sorted(os.walk(dir)):
 		for fname in fnames:
 			if is_image_file(fname):
 				path = os.path.join(root, fname)
-				path_names = path.split('/') 
-				print(path_names)
-
-				path_names = path_names[2:]
-				del path_names[1]
+				path1 = path.replace('fashion_data/img', 'fashion')
+				path_names = path1.split('/') 
+				# path_names[2] = path_names[2].replace('_', '')
 				path_names[3] = path_names[3].replace('_', '')
 				path_names[4] = path_names[4].split('_')[0] + "_" + "".join(path_names[4].split('_')[1:])
 				path_names = "".join(path_names)
-				# img = Image.open(path)
-				if path_names in train_images:
-					shutil.copy(path, os.path.join(train_root, path_names))
-					print(os.path.join(train_root, path_names))
-# 					pass
-				elif path_names in test_images:
-					shutil.copy(path, os.path.join(test_root, path_names))
-					print(os.path.join(train_root, path_names))
-# 					pass
-
-make_dataset('./fashion_data')
+				print(path_names)
+				new_path = path_names
+				img = Image.open(path)
+				#imgcrop = img.crop((40, 0, 216, 256))
+				if new_path in train_images:
+					img.save(os.path.join(train_root, path_names))
+				elif new_path in test_images:
+					img.save(os.path.join(test_root, path_names))
+# 					
+make_dataset('fashion_data/img')
